285d0876ec93d3358d417f7fa5f5cfab69861d49
Zameer Hussain <zameer.vighio@hotmail.com>
2023-03-31 14:34:11 +0500

    Step03:- DI & Strategy Pattern.

--- IoC/Step3DI.cs
+++ IoC/Step3DI.cs
@@ -6,6 +6,34 @@ using System.Threading.Tasks;
 
 namespace IoC_and_DI.IoC
 {
+
+    /*
+     * In the previous chapter, related to DIP, we created and used abstraction to make the classes loosely coupled. 
+     * Here, we are going to implement Dependency Injection and strategy pattern together to move the dependency object creation completely out of the class. 
+     * This is our third step in making the classes completely loose coupled....
+     * 
+     * *******************************************************************
+     * Dependency Injection (DI) is a design pattern used to implement IoC
+     *  It allows the creation of dependent objects outside of a class and provides those objects to a class through different ways. 
+     *  Using DI, we move the creation and binding of the dependent objects outside of the class that depends on them.
+     *  Dependency Injection Pattern involves 3 types of class
+     *  Client Class:- (Dependent class) that depend on Server class.
+     *  Server Class:- (Dependency) is a class that provides service to the client class
+     *  Injector Class:- this injects the service class object into the client class.
+     *  
+     *  The injector class creates an object of the service class, and injects that object to a client object
+     *  This way DI pattern separates the responsibility of creating an object of the service class out of client class.
+     *  ***********************************************************************************************
+     *  
+     *  
+     *  Dependency Injection injects dependencies broadly in three ways
+     *  01:- Constructor Injection: - Injector supplies services(dependencies) through the client class constructor.
+     *  02:- Property Injection: - Injector supplies the dependency through public property of client class.
+     *  03:- Method Injection: - in this type, client class implements an interface which declares the method(s) to supply the dependency and the injector uses this interface to supply the dependency to the client class...
+     *  
+     *  For examples visit
+     *  https://www.tutorialsteacher.com/ioc/dependency-injection
+     */
     internal class Step3DI
     {
     }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using IoC_and_DI.IoC;
 
-Console.WriteLine("Hello, World!");
+/*
+ * All work copied from : Tutorial Teacher
+ * https://www.tutorialsteacher.com/ioc/introduction
+ */
 
 //calling simple tightly coupled class without any step.
 A a = new A();
@@ -20,6 +23,8 @@ a1.task2();
 //now, let's understand it with more practical way, 
 //loosly coupled means:- changes in one class should not force other classes to change.
 //so whole application become maintainable and extensible.
+//STEP:01:- Inversion of Control, implementing Factory
+//https://www.tutorialsteacher.com/ioc/inversion-of-control
 BussinessLogic blogic = new BussinessLogic();
 blogic.PrintCustomerName(2);
 
@@ -30,9 +35,14 @@ blogic.PrintCustomerName(2);
 // creation to the factory class.
 //Let 's use DIP on the BusinessLogic and DataAccessor classes and make them more loosely coupled...
 //STEP#02:- DIP, Hight level should not depend on Low level, both depend on Abstraction.
+//https://www.tutorialsteacher.com/ioc/dependency-inversion-principle
 BussinessLogicDIP dip = new BussinessLogicDIP();
 dip.PrintCustomerName(100);
 
+
+
+//STEP03:- Dependency Injection & Strategy Pattern
+//https://www.tutorialsteacher.com/ioc/dependency-injection
 
 
 
